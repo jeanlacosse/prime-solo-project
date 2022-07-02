8f5e0ee730e8c5bcddfa08ac438cd3c177f4f627
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-07-02 08:40:59 -0500

    building out the questions

---
+++ src/components/Ratings/Ratings.jsx
@@ -0,0 +1,14 @@
+function Ratings () {
+    return (
+        <>
+        {/* TODO
+        turn this into a few questions that can be answered on a sliding scale
+        place accross multiple pages/routes
+        have it go to a success page
+        */}
+        <h3>hello world</h3>
+        </>
+    )
+}
+
+export default Ratings;
