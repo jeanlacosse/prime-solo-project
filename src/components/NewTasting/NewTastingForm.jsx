d2533372a6b0157da7cfdda02332c492d1747786
Jean-Luc LaCosse <jeanlacosse@gmail.com>
2022-07-05 15:57:33 -0500

    form for wine info completed

--- src/components/NewTasting/NewTastingForm.jsx
+++ src/components/NewTasting/NewTastingForm.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 function NewTastingForm() {
 
     const dispatch = useDispatch();
+    const history = useHistory();
+
+    // this will set todays date in the input field
+    const today = new Date();
+    const date = today.setDate(today.getDate());
+    const todaysDate = new Date(date).toISOString().split('T')[0] // yyyy-mm-dd
+
 
     // this is the local state the wine information is stored inside
-    let [newWineInfo, setNewWineInfo] = useState({})
+    let [newWineInfo, setNewWineInfo] = useState({
+        date: todaysDate
+    })
 
 
     const handleWineInfo = (event) => {
@@ -16,26 +27,24 @@ function NewTastingForm() {
     }
     const addWineInformation = (event) => {
         event.preventDefault();
-
+        console.log(newWineInfo)
         // this will dispatch the local state to the saga to be placed in store
     }
 
-    // this will set todays date in the input field
-    const today = new Date();
-    const date = today.setDate(today.getDate());
-    const todaysDate = new Date(date).toISOString().split('T')[0] // yyyy-mm-dd
-
     
+
     return (
         <>
             <h2>Please enter the wines information below</h2>
             <form onSubmit={(event) => addWineInformation(event)}>
                 {/* this will set the date to todays date on page load, changeable though */}
 
-                < input 
-                type="date" 
-                name="date" 
-                defaultValue = {todaysDate} 
+                < input
+                    type="date"
+                    name="date"
+                    defaultValue={todaysDate}
+                    onChange={handleWineInfo}
+                    required
                 />
 
                 <input
@@ -44,6 +53,7 @@ function NewTastingForm() {
                     type='text'
                     placeholder='Winery Name'
                     value={newWineInfo.winery_name}
+                    required
                 />
 
                 <input
@@ -52,14 +62,16 @@ function NewTastingForm() {
                     type='text'
                     placeholder='Varietal of Grape'
                     value={newWineInfo.varietal}
+                    required
                 />
 
                 <input
                     onChange={handleWineInfo}
                     name="vintage"
-                    type='text'
+                    type='number'
                     placeholder='Vintage/Year Produced'
                     value={newWineInfo.vintage}
+                    required
                 />
 
                 <input
@@ -68,10 +80,14 @@ function NewTastingForm() {
                     type='text'
                     placeholder='Region Wine Was Produced'
                     value={newWineInfo.region}
+                    required
                 />
 
                 <button type='submit'>Next</button>
             </form>
+            <button
+                onClick={() => history.push('/home')}
+            >Back</button>
         </>
     )
 };
